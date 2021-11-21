60e8875e17696a1f1c15a3e407cca585ba6c17ae
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2021-11-21 11:29:05 +0900

    Registration2コミット

---
+++ Registration2/src/main/java/com/example/demo/entity/RegistData.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class RegistData {
+
+	private String name;
+	private String password;
+	private int gender;
+	private int area;
+	private int[] interest;
+	private String remarks;
+}
